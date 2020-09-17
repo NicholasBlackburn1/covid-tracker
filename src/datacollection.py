443b04c9442405b3a55bb52aa19118361091e345
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-09-17 08:55:51 -0400

    added more libs

--- src/datacollection.py
+++ src/datacollection.py
@@ -1,1 +1,20 @@
-v
+from covid import Covid
+import numpy as np
+from pandas.io.json import json_normalize 
+import matplotlib as mat
+import json
+"""
+This python class is for grabing data and organizing the data 
+@author nicholas blackburn
+"""
+covid = Covid()
+
+countries = covid.list_countries()
+
+italy_cases = covid.get_status_by_country_name("italy")
+usa_cases = covid.get_status_by_country_name("US")
+china_cases = covid.get_status_by_country_name("china")
+
+def getData(Location):
+    json_parse = json.loads(Location)
+    
